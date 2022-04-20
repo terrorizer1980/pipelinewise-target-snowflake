b83e16f4f19b17e5497610c6b44a7cea16972a26
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-04-20 16:54:53 +0200

    Merge records with the same primary key instead of replacing them

--- target_snowflake/__init__.py
+++ target_snowflake/__init__.py
@@ -167,11 +167,16 @@ def persist_lines(config, lines, table_cache=None, file_format_type: FileFormatT
                 row_count[stream] += 1
                 total_row_count[stream] += 1
 
-            # append record
+            # set the record default to an empty dict
+            current_record =  records_to_load[stream].get(primary_key_string)
+            if current_record is None:
+                records_to_load[stream][primary_key_string] = current_record = {}
+
+            # merge the incoming record into the current record
             if config.get('add_metadata_columns') or config.get('hard_delete'):
-                records_to_load[stream][primary_key_string] = stream_utils.add_metadata_values_to_record(o)
+                records_to_load[stream][primary_key_string] = {**current_record, **stream_utils.add_metadata_values_to_record(o)}
             else:
-                records_to_load[stream][primary_key_string] = o['record']
+                records_to_load[stream][primary_key_string] = {**current_record, **o['record']}
 
             if archive_load_files and stream in archive_load_files_data:
                 # Keep track of min and max of the designated column
